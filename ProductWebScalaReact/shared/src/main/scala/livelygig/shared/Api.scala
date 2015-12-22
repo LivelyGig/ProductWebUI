b66a23482080c00394c65be6adcfaa540ee8aa15
Shubham Kamthania <shubham.kamthania@afourtech.com>
2015-12-22 15:59:33 +0530

    Addding backend method/dtos for email validation and user login.

--- ProductWebScalaReact/shared/src/main/scala/livelygig/shared/Api.scala
+++ ProductWebScalaReact/shared/src/main/scala/livelygig/shared/Api.scala
@@ -5,18 +5,6 @@ import livelygig.shared.dtos._
 import scala.concurrent.Future
 
 trait Api {
-//  // message of the day
-//  def welcome(name: String): String
-//
-//  // get Todo items
-//  def getTodos(): Seq[TodoItem]
-//
-//  // update a Todo
-//  def updateTodo(item: TodoItem): Seq[TodoItem]
-//
-//  // delete a Todo
-//  def deleteTodo(itemId: String): Seq[TodoItem]
-
   def createAgent(userRequest: CreateUserRequest): Future[String]
 
   def confirmEmail(confirmEmailRequest: ConfirmEmailRequest): Future[String]
