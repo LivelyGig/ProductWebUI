970fae553ca8e8bed646d5c6eebeb872a9a0e6ed
Shubham Kamthania <shubham.kamthania@afourtech.com>
2016-01-12 11:54:31 +0530

    Adding DIODE as library dependency.

---
+++ ProductWebScalaReact/client/src/main/scala/livelygig/client/services/LGCircuit.scala
@@ -0,0 +1,19 @@
+package livelygig.client.services
+
+import autowire._
+import diode._
+import diode.data._
+import diode.util._
+import diode.react.ReactConnector
+import boopickle.Default._
+import scala.scalajs.concurrent.JSExecutionContext.Implicits.runNow
+
+/**
+  * Created by shubham.k on 1/11/2016.
+  */
+
+// Actions
+
+object LGCircuit {
+
+}
