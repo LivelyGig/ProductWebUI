848a38c0e45ac37c9c9d89381d1177d32116970e
Ed Eykholt <ed@livelygig.com>
2016-01-19 15:03:28 -0800

    - Changed/added some buttons for each connection.   Message, Introduce, Recommend, Favorite, Hide.

--- ProductWebScalaReact/client/src/main/scala/livelygig/client/modules/Connections.scala
+++ ProductWebScalaReact/client/src/main/scala/livelygig/client/modules/Connections.scala
@@ -104,8 +104,11 @@ object ConnectionList {
           <.div(^.className:="media-body")(
             "lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
             <.div(^.className:="col-md-12 col-sm-12")(
-              <.button(HeaderCSS.Style.rsltContainerBtn, ^.className:="btn")("Recommended")(),
-              <.button(HeaderCSS.Style.rsltContainerBtn, ^.className:="btn")("Reply")())
+              <.button(HeaderCSS.Style.rsltContainerBtn, ^.className:="btn")("Hide")(),
+              <.button(HeaderCSS.Style.rsltContainerBtn, ^.className:="btn")("Favorite")(),
+              <.button(HeaderCSS.Style.rsltContainerBtn, ^.className:="btn")("Recommend")(),
+              <.button(HeaderCSS.Style.rsltContainerBtn, ^.className:="btn")("Introduce")(),
+              <.button(HeaderCSS.Style.rsltContainerBtn, ^.className:="btn")("Message")())
           )//media-body
         )
 
