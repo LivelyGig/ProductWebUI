905b7f1a7ddc29e863b7fec7efaf4de22df4d2dd
Ed Eykholt <ed@livelygig.com>
2016-01-24 18:38:08 -0800

    - added image to indicate ability to sign in (or at least register) with social/professional credentials. - changed some of the connection results, for discussion.

--- ProductWebScalaReact/client/src/main/scala/livelygig/client/Modals/LoginForm.scala
+++ ProductWebScalaReact/client/src/main/scala/livelygig/client/Modals/LoginForm.scala
@@ -10,20 +10,21 @@ import org.scalajs.dom._
 
 import scalacss.ScalaCssReact._
 
-object LoginForm {   //TodoForm
-// shorthand fo
-@inline private def bss = GlobalStyles.bootstrapStyles
+object LoginForm {
+  //TodoForm
+  // shorthand fo
+  @inline private def bss = GlobalStyles.bootstrapStyles
 
   case class Props(submitHandler: (AgentLoginModel, Boolean, Boolean, Boolean) => Callback)
 
-  case class State(agentloginModel: AgentLoginModel, login: Boolean= false, showConfirmAccountCreation: Boolean = false,
+  case class State(agentloginModel: AgentLoginModel, login: Boolean = false, showConfirmAccountCreation: Boolean = false,
                    showNewAgentForm: Boolean = false)
 
   class Backend(t: BackendScope[Props, State]) {
-    def submitForm(e:ReactEventI) = {
+    def submitForm(e: ReactEventI) = {
       e.preventDefault()
-     t.modState(s => s.copy(login = true))
-    //  jQuery(t.getDOMNode()).modal("hide")
+      t.modState(s => s.copy(login = true))
+      //  jQuery(t.getDOMNode()).modal("hide")
     }
 
     def hide = {
@@ -31,16 +32,20 @@ object LoginForm {   //TodoForm
       // instruct Bootstrap to hide the modal
       jQuery(t.getDOMNode()).modal("hide")
     }
-    def updateEmail(e:ReactEventI) = {
+
+    def updateEmail(e: ReactEventI) = {
       t.modState(s => s.copy(agentloginModel = s.agentloginModel.copy(email = e.target.value)))
     }
-    def showValidate(e:ReactEventI) = {
+
+    def showValidate(e: ReactEventI) = {
       t.modState(s => s.copy(showConfirmAccountCreation = true))
     }
-    def showAddNewAgent(e:ReactEventI) = {
+
+    def showAddNewAgent(e: ReactEventI) = {
       t.modState(s => s.copy(showNewAgentForm = true))
     }
-    def updatePassword(e:ReactEventI) = {
+
+    def updatePassword(e: ReactEventI) = {
       t.modState(s => s.copy(agentloginModel = s.agentloginModel.copy(password = e.target.value)))
     }
 
@@ -59,45 +64,60 @@ object LoginForm {   //TodoForm
         header = hide => <.span(<.button(^.tpe := "button", bss.close, ^.onClick --> hide, Icon.close), <.div(DashBoardCSS.Style.modalHeaderText)(headerText)),
         closed = () => formClosed(s, p)),
         <.form(^.onSubmit ==> submitForm)(
-          <.div(^.className:="row")(
-            <.div(^.className:="col-md-12 col-sm-12 col-xs-12")(
-              <.div(DashBoardCSS.Style.scltInputModalContainerMargin)(
-             //   <.div(DashBoardCSS.Style.modalHeaderFont)("Login"),
-                <.input(^.tpe := "text", bss.formControl, DashBoardCSS.Style.inputModalMargin ,^.id := "Name",
-                  ^.placeholder:="username", ^.value:= s.agentloginModel.email, ^.onChange==>updateEmail, ^.required:=true),
-                <.input(^.tpe := "password", bss.formControl, DashBoardCSS.Style.inputModalMargin , ^.placeholder:="password"
-                  , ^.value:= s.agentloginModel.password, ^.onChange==>updatePassword, ^.required:=true) ,
-                <.button(^.tpe := "submit",^.className:="btn btn-default",DashBoardCSS.Style.btnWidth,"Login")),
+          <.div(^.className := "row")(
+            //left
+            <.div(^.className := "col-md-7 col-sm-7 col-xs-7")(
 
-              <.div(^.className:="row",DashBoardCSS.Style.scltInputModalContainerMargin,DashBoardCSS.Style.marginTop10px)(
-                <.div(^.className:="col-md-4",DashBoardCSS.Style.paddingLeftLoginbtn)(
-                  <.button(^.tpe := "button",^.className:="btn btn-default",DashBoardCSS.Style.btnWidth,"Validate Account",
-                    ^.onClick==>showValidate)
-                ),
-                <.div(^.className:="col-md-4")(
-                  <.button(^.tpe := "button",^.className:="btn btn-default",DashBoardCSS.Style.btnWidth,"Sign Up",
-                    ^.onClick==>showAddNewAgent)
-                ),
-                <.div(^.className:="col-md-4",DashBoardCSS.Style.paddingRightLoginbtn)(
-                  <.button(^.tpe := "button",^.className:="btn btn-default",DashBoardCSS.Style.btnWidth,"Forgot My Password",
-                    ^.onClick==>showValidate)
+              <.div(/*DashBoardCSS.Style.scltInputModalContainerMargin */)(
+                <.div(DashBoardCSS.Style.modalHeaderFont)("Sign in with LivelyGig credentials"),
+                <.input(^.tpe := "text", bss.formControl, /* DashBoardCSS.Style.inputModalMargin, */ ^.id := "Name",
+                  ^.placeholder := "username", ^.value := s.agentloginModel.email, ^.onChange ==> updateEmail, ^.required := true),
+                <.input(^.tpe := "password", bss.formControl, DashBoardCSS.Style.inputModalMargin, ^.placeholder := "password"
+                  , ^.value := s.agentloginModel.password, ^.onChange ==> updatePassword, ^.required := true),
+                <.button(^.tpe := "submit", ^.className := "btn btn-default", DashBoardCSS.Style.btnWidth, "Login"),
+
+                <.div(^.paddingTop := "10px")(
+                  <.div(^.className := "col-md-4 col-sm-4 col-xs-4")(
+                    <.button(^.tpe := "button", ^.className := "btn btn-default", /* DashBoardCSS.Style.btnWidth,*/ "Validate Account",
+                      ^.onClick ==> showValidate)
+                  ),
+                  <.div(^.className := "col-md-3 col-sm-3 col-xs-3")(
+                    <.button(^.tpe := "button", ^.className := "btn btn-default", /* DashBoardCSS.Style.btnWidth,*/ "Sign Up",
+                      ^.onClick ==> showAddNewAgent)
+                  ),
+                  <.div(^.className := "col-md-4 col-sm-4 col-xs-4")(
+                    <.button(^.tpe := "button", ^.className := "btn btn-default", /* DashBoardCSS.Style.btnWidth,*/ "Forgot My Password",
+                      ^.onClick ==> showValidate)
+                  )
                 )
-              ),
-              <.div(bss.modal.footer,DashBoardCSS.Style.marginTop10px,DashBoardCSS.Style.marginLeftRight)()
+              )
+            ),
+            // right
+
+            <.div(^.className := "col-md-5 col-sm-5 col-xs-5", ^.borderLeft := "solid")(
+              <.div(DashBoardCSS.Style.modalHeaderFont)("Sign in with shared credentials"),
+              // example, e.g. from https://www.import.io
+              <.img()(^.src := "./assets/images/sampleSocialLogin.png")
             )
           )
-        )
+        ),
+
+
+        <.div(bss.modal.footer, DashBoardCSS.Style.marginTop10px, DashBoardCSS.Style.marginLeftRight)()
       )
+
     }
   }
+
   private val component = ReactComponentB[Props]("AddLoginForm")
-    .initialState_P(p => State(new AgentLoginModel("","")))
+    .initialState_P(p => State(new AgentLoginModel("", "")))
     .renderBackend[Backend]
-    .componentDidUpdate(scope => Callback{
-     if (scope.currentState.login || scope.currentState.showConfirmAccountCreation || scope.currentState.showNewAgentForm) {
-                scope.$.backend.hide
+    .componentDidUpdate(scope => Callback {
+      if (scope.currentState.login || scope.currentState.showConfirmAccountCreation || scope.currentState.showNewAgentForm) {
+        scope.$.backend.hide
       }
     })
     .build
+
   def apply(props: Props) = component(props)
 }
--- ProductWebScalaReact/client/src/main/scala/livelygig/client/modules/Connections.scala
+++ ProductWebScalaReact/client/src/main/scala/livelygig/client/modules/Connections.scala
@@ -12,9 +12,12 @@ import livelygig.client.services.{ConnectionsRootModel, RefreshConnections}
 import scalacss.ScalaCssReact._
 
 object Connections {
+
   case class Props(proxy: ModelProxy[Pot[ConnectionsRootModel]])
+
   case class State(selectedItem: Option[ConnectionsModel] = None)
-  class Backend($: BackendScope[Props, State]){
+
+  class Backend($: BackendScope[Props, State]) {
     def mounted(props: Props) =
       Callback.ifTrue(props.proxy().isEmpty, props.proxy.dispatch(RefreshConnections))
   }
@@ -24,40 +27,40 @@ object Connections {
     .initialState(State())
     .backend(new Backend(_))
     .renderPS(($, P, S) => {
-      <.div(^.id := "mainContainer"/*, DashBoardCSS.Style.mainContainerDiv*/, ^.className:="mainContainerDiv")(
+      <.div(^.id := "mainContainer" /*, DashBoardCSS.Style.mainContainerDiv*/ , ^.className := "mainContainerDiv")(
         <.div()(
           ConnectionsPresets.component(Unit)
         ),
         // AddNewAgent(AddNewAgent.Props(ctl)),
-        <.div(/*DashBoardCSS.Style.splitContainer*/^.className:="splitContainer")(
+        <.div(/*DashBoardCSS.Style.splitContainer*/ ^.className := "splitContainer")(
           <.div(^.className := "split")(
             <.div(^.className := "row")(
               <.div(^.className := "col-md-2 col-sm-2 col-xs-2")(
                 // todo: Need to parameterize the Search area depending on EntityType (e.g. Talent, Project) and preset
                 ConnectionsSearch.component(Unit)
               ),
-              <.div(^.className := "col-md-10 col-sm-10 col-xs-10",^.id := "resultsContainer",^.id := "dashboardResults2",^.className:="dashboardResults2")(
-                <.div(^.id:="rsltScrollContainer", DashBoardCSS.Style.rsltContainer)(
-                  <.div(^.className:="container-fluid", ^.id:="resultsConnectionsContainer" )(
-                    <.div (^.id:="rsltSectionContainer", ^.className:="col-md-12 col-sm-12 col-xs-12", ^.paddingLeft:="0px", ^.paddingRight:="0px")(
-                // todo: Results will be parameterized depending on EntityType, preset
-                P.proxy().render(connectionsRootModel =>
-                  ConnectionList(connectionsRootModel.connectionsResponse)
-                )
-                /*Panel(Panel.Props("Connections"), <.div(
-                  P.proxy().renderFailed(ex => "Error loading"),
-                  P.proxy().renderPending(_ > 500, _ => "Loading..."),
-                  P.proxy().render(connectionsRootModel =>
-                    ConnectionList(connectionsRootModel.connectionsResponse)
-                  )
-                ))*/
-                   )
+              <.div(^.className := "col-md-10 col-sm-10 col-xs-10", ^.id := "resultsContainer", ^.id := "dashboardResults2", ^.className := "dashboardResults2")(
+                <.div(^.id := "rsltScrollContainer", DashBoardCSS.Style.rsltContainer)(
+                  <.div(^.className := "container-fluid", ^.id := "resultsConnectionsContainer")(
+                    <.div(^.id := "rsltSectionContainer", ^.className := "col-md-12 col-sm-12 col-xs-12", ^.paddingLeft := "0px", ^.paddingRight := "0px")(
+                      // todo: Results will be parameterized depending on EntityType, preset
+                      P.proxy().render(connectionsRootModel =>
+                        ConnectionList(connectionsRootModel.connectionsResponse)
+                      )
+                      /*Panel(Panel.Props("Connections"), <.div(
+                        P.proxy().renderFailed(ex => "Error loading"),
+                        P.proxy().renderPending(_ > 500, _ => "Loading..."),
+                        P.proxy().render(connectionsRootModel =>
+                          ConnectionList(connectionsRootModel.connectionsResponse)
+                        )
+                      ))*/
+                    )
                   )
                 )
               )
             )
           )
-        )//row
+        ) //row
       ) //mainContainer
     })
     .componentDidMount(scope => scope.backend.mounted(scope.props))
@@ -68,57 +71,67 @@ object Connections {
 }
 
 
-
 object ConnectionList {
+
   case class ConnectionListProps(connections: Seq[ConnectionsModel])
 
   private val ConnectionList = ReactComponentB[ConnectionListProps]("ConnectionList")
     .render_P(p => {
       def renderConnections(connection: ConnectionsModel) = {
-//        <.li(
-//          //         <.span( connection.content.jsonBlobModel.get.name)
-//          if (!connection.name.isEmpty){
-//            <.div(^.className:="col-md-12")(connection.name)
-//          }
-//          else {
-//            <.span()
-//          }
-//
-//        )
+        //        <.li(
+        //          //         <.span( connection.content.jsonBlobModel.get.name)
+        //          if (!connection.name.isEmpty){
+        //            <.div(^.className:="col-md-12")(connection.name)
+        //          }
+        //          else {
+        //            <.span()
+        //          }
+        //
+        //        )
 
-        <.li(^.className:="media", DashBoardCSS.Style.rsltContentBackground, DashBoardCSS.Style.rsltpaddingTop10p)(
-          <.input(^.`type` := "checkbox",DashBoardCSS.Style.rsltCheckboxStyle),
-          <.span(^.className:="checkbox-lbl"),
-          if (!connection.name.isEmpty){
-          <.div (DashBoardCSS.Style.profileNameHolder )(connection.name)
-          }  else {
+        <.li(^.className := "media", DashBoardCSS.Style.rsltContentBackground, DashBoardCSS.Style.rsltpaddingTop10p)(
+          <.input(^.`type` := "checkbox", DashBoardCSS.Style.rsltCheckboxStyle),
+          <.span(^.className := "checkbox-lbl"),
+          if (!connection.name.isEmpty) {
+            <.div(DashBoardCSS.Style.profileNameHolder)(connection.name)
+          } else {
             <.span()
           },
-          <.div(^.className:="col-md-12")(
-            <.div (DashBoardCSS.Style.rsltProfileDetailsHolder)("Experience: 8 years"),
-            <.div (DashBoardCSS.Style.rsltProfileDetailsHolder)("Projects Completed: 24"),
-            <.div (DashBoardCSS.Style.rsltProfileDetailsHolder)("Availability: Negotiable")),
-          <.div(^.className:="media-left")(
+          <.div(^.className := "col-md-12")(
+            <.div(DashBoardCSS.Style.rsltProfileDetailsHolder)("Software Developer"),
+            <.div(DashBoardCSS.Style.rsltProfileDetailsHolder)("Pune, India"),
+            <.div(DashBoardCSS.Style.rsltProfileDetailsHolder)("Connected since 2014-01-02"),
+            <.div(DashBoardCSS.Style.rsltProfileDetailsHolder)("Profiles: ",
+              <.a()(^.href:="", "title".reactAttr :="Videographer")("Videographer"),
+              "| ",
+              <.a()(^.href:="", "title".reactAttr :="Web Developer")("Web Developer"),
+              "| ",
+              <.a()(^.href:="", "title".reactAttr :="Janal, LLC")("Janal, LLC")
+            )
+
+          ),
+          <.div(^.className := "media-left")(
             <.img(DashBoardCSS.Style.profileImg, ^.src := "./assets/images/profile-img.png")
           ), //media-left
-          <.div(^.className:="media-body")(
-            "lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-            <.div(^.className:="col-md-12 col-sm-12")(
-              <.button(HeaderCSS.Style.rsltContainerBtn, ^.className:="btn")("Hide")(),
-              <.button(HeaderCSS.Style.rsltContainerBtn, ^.className:="btn")("Favorite")(),
-              <.button(HeaderCSS.Style.rsltContainerBtn, ^.className:="btn")("Recommend")(),
-              <.button(HeaderCSS.Style.rsltContainerBtn, ^.className:="btn")("Introduce")(),
-              <.button(HeaderCSS.Style.rsltContainerBtn, ^.className:="btn")("Message")())
-          )//media-body
+          <.div(^.className := "media-body")(
+            //*"lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",*
+            <.div(^.className := "col-md-12 col-sm-12")(
+              <.button(HeaderCSS.Style.rsltContainerBtn, ^.className := "btn")("Hide")(),
+              <.button(HeaderCSS.Style.rsltContainerBtn, ^.className := "btn")("Favorite")(),
+              <.button(HeaderCSS.Style.rsltContainerBtn, ^.className := "btn")("Recommend")(),
+              <.button(HeaderCSS.Style.rsltContainerBtn, ^.className := "btn")("Introduce")(),
+              <.button(HeaderCSS.Style.rsltContainerBtn, ^.className := "btn")("Message")()
+            )
+          ) //media-body
         )
 
 
-
       }
-      <.ul(^.className:="media-list")(p.connections map renderConnections)
+      <.ul(^.className := "media-list")(p.connections map renderConnections)
 
     })
     .build
+
   def apply(connections: Seq[ConnectionsModel]) =
     ConnectionList(ConnectionListProps(connections))
 }
