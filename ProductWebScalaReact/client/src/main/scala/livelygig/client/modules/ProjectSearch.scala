cabd0c51320cc6b9fca90874bb14ef0cd9bb84cb
Ed Eykholt <ed@livelygig.com>
2016-01-27 14:37:19 -0800

    - minor change.  "Fixed Scope"

--- ProductWebScalaReact/client/src/main/scala/livelygig/client/modules/ProjectSearch.scala
+++ ProductWebScalaReact/client/src/main/scala/livelygig/client/modules/ProjectSearch.scala
@@ -14,81 +14,80 @@ object ProjectSearch {
   // create the React component for Dashboard
   val component = ReactComponentB[RouterCtl[Loc]]("Projects")
     .render_P(ctl =>
-      // todo: Need to parameterize on type (e.g. Talent, Project) and preset (e.g. Recommended Mathces)
-      "talentPreset1" match {
-        case "talentPreset1" =>
-          <.div(^.id := "slctScrollContainer", DashBoardCSS.Style.slctContainer)(
-            <.div(LftcontainerCSS.Style.fontsize12em, LftcontainerCSS.Style.slctsearchpanelabelposition)(
-              <.div(DashBoardCSS.Style.slctHeaders)("Search Criteria"),
-              <.div(DashBoardCSS.Style.slctHeaders)("Job Type:"),
 
-                <.label(DashBoardCSS.Style.slctCheckboxesLabel)(
-                  <.input(^.`type` := "checkbox", ^.id:="jobTypeCheckboxProject"), " Project"),
-                <.div(DashBoardCSS.Style.slctSubCheckboxesDiv)(
-                  <.label(DashBoardCSS.Style.slctSubCheckboxesLabel)(
-                    <.input(^.`type` := "checkbox", ^.id:="jobTypeCheckboxHourly"), " Hourly"),
-                  <.br(),
-                  <.label(DashBoardCSS.Style.slctSubCheckboxesLabel)(
-                    <.input(^.`type` := "checkbox", ^.value:=true, ^.id:="jobTypeCheckboxFixed"), " Fixed Time and/or Scope"),
-                  <.br()
-                ),
-              <.label(DashBoardCSS.Style.slctCheckboxesLabel)(
-                  <.input(^.`type` := "checkbox"), " Contest"),
+      <.div(^.id := "slctScrollContainer", DashBoardCSS.Style.slctContainer)(
+        <.div(LftcontainerCSS.Style.fontsize12em, LftcontainerCSS.Style.slctsearchpanelabelposition)(
+          <.div(DashBoardCSS.Style.slctHeaders)("Search Criteria"),
+          <.div(DashBoardCSS.Style.slctHeaders)("Job Type:"),
+
+          <.label(DashBoardCSS.Style.slctCheckboxesLabel)(
+            <.input(^.`type` := "checkbox", ^.id := "jobTypeCheckboxProject"), " Project"),
+          <.div(DashBoardCSS.Style.slctSubCheckboxesDiv)(
+            <.label(DashBoardCSS.Style.slctSubCheckboxesLabel)(
+              <.input(^.`type` := "checkbox", ^.id := "jobTypeCheckboxHourly"), " Hourly"),
+            <.br(),
+            <.label(DashBoardCSS.Style.slctSubCheckboxesLabel)(
+              <.input(^.`type` := "checkbox", ^.value := true, ^.id := "jobTypeCheckboxFixed"), " Fixed Scope"),
+            <.br()
+          ),
+          <.label(DashBoardCSS.Style.slctCheckboxesLabel)(
+            <.input(^.`type` := "checkbox"), " Contest"),
 
 
-              <.div(^.className := "row")(
-                <.div(^.className := "col-md-12 col-sm-12 col-xs-12", ProjectCSS.Style.slctProjectInputWidth)(
-                  <.div("Start Date")
-                  //                        <.span(^.className:="checkbox-lbl")
+          <.div(^.className := "row")(
+            <.div(^.className := "col-md-12 col-sm-12 col-xs-12", ProjectCSS.Style.slctProjectInputWidth)(
+              <.div("Start Date")
+              //                        <.span(^.className:="checkbox-lbl")
+            ),
+            <.div(MessagesCSS.Style.slctMessagesInputLeftContainerMargin)(
+              <.input(^.className := "form-control", DashBoardCSS.Style.inputHeightWidth)
+            )
+          ),
+          <.div(^.className := "row")(
+            <.div(^.className := "col-md-12 col-sm-12 col-xs-12", ProjectCSS.Style.slctProjectInputWidth)(
+              <.div("End Date")
+              //                        <.span(^.className:="checkbox-lbl")
+            ),
+            <.div(MessagesCSS.Style.slctMessagesInputLeftContainerMargin)(
+              <.input(^.className := "form-control", DashBoardCSS.Style.inputHeightWidth)
+            )
+          ),
+          <.div(^.className := "row")(
+            <.div(^.className := "col-md-12 col-sm-12 col-xs-12", ProjectCSS.Style.slctProjectInputWidth)(
+              <.div("Skill Required")
+              //                        <.span(^.className:="checkbox-lbl")
+            ),
+            <.div(MessagesCSS.Style.slctMessagesInputLeftContainerMargin)(
+              //                  <.input(^.className:="form-control", DashBoardCSS.Style.inputHeightWidth),
+              <.input(^.`type` := "textarea", ProjectCSS.Style.textareaWidth, ^.lineHeight := 4, DashBoardCSS.Style.marginTop10px)
+            )
+          ),
+          <.div(^.className := "row")(
+            <.div(^.className := "col-md-12 col-sm-12 col-xs-12", ProjectCSS.Style.slctProjectInputWidth)(
+              <.div("Project State")
+              //                        <.span(^.className:="checkbox-lbl")
+            ),
+            <.div(MessagesCSS.Style.slctMessagesInputLeftContainerMargin, DashBoardCSS.Style.marginTop10px)(
+              //<.input(^.className:="form-control", DashBoardCSS.Style.inputHeightWidth)
+              <.div(^.className := "btn-group")(
+                <.button(ProjectCSS.Style.projectdropdownbtn, ^.className := "btn dropdown-toggle", "data-toggle".reactAttr := "dropdown")("Select One")(
+                  <.span(^.className := "caret")
                 ),
-                <.div(MessagesCSS.Style.slctMessagesInputLeftContainerMargin)(
-                  <.input(^.className := "form-control", DashBoardCSS.Style.inputHeightWidth)
+                <.ul(^.className := "dropdown-menu")(
+                  <.li()(<.a(^.href := "#")("Item 1")),
+                  <.li()(<.a(^.href := "#")("Item 2")),
+                  <.li()(<.a(^.href := "#")("Item 3"))
                 )
-              ),
-              <.div(^.className := "row")(
-                <.div(^.className := "col-md-12 col-sm-12 col-xs-12", ProjectCSS.Style.slctProjectInputWidth)(
-                  <.div("End Date")
-                  //                        <.span(^.className:="checkbox-lbl")
-                ),
-                <.div(MessagesCSS.Style.slctMessagesInputLeftContainerMargin)(
-                  <.input(^.className := "form-control", DashBoardCSS.Style.inputHeightWidth)
-                )
-              ),
-              <.div(^.className := "row")(
-                <.div(^.className := "col-md-12 col-sm-12 col-xs-12", ProjectCSS.Style.slctProjectInputWidth)(
-                  <.div("Skill Required")
-                  //                        <.span(^.className:="checkbox-lbl")
-                ),
-                <.div(MessagesCSS.Style.slctMessagesInputLeftContainerMargin)(
-                  //                  <.input(^.className:="form-control", DashBoardCSS.Style.inputHeightWidth),
-                  <.input(^.`type` := "textarea", ProjectCSS.Style.textareaWidth, ^.lineHeight := 4, DashBoardCSS.Style.marginTop10px)
-                )
-              ),
-              <.div(^.className := "row")(
-                <.div(^.className := "col-md-12 col-sm-12 col-xs-12", ProjectCSS.Style.slctProjectInputWidth)(
-                  <.div("Project State")
-                  //                        <.span(^.className:="checkbox-lbl")
-                ),
-                <.div(MessagesCSS.Style.slctMessagesInputLeftContainerMargin, DashBoardCSS.Style.marginTop10px)(
-                  //<.input(^.className:="form-control", DashBoardCSS.Style.inputHeightWidth)
-                  <.div(^.className := "btn-group")(
-                    <.button(ProjectCSS.Style.projectdropdownbtn, ^.className := "btn dropdown-toggle", "data-toggle".reactAttr := "dropdown")("Select One")(
-                      <.span(^.className := "caret")
-                    ),
-                    <.ul(^.className := "dropdown-menu")(
-                      <.li()(<.a(^.href := "#")("Item 1")),
-                      <.li()(<.a(^.href := "#")("Item 2")),
-                      <.li()(<.a(^.href := "#")("Item 3"))
-                    )
-                  )
-                )
-              ),
-              <.div(DashBoardCSS.Style.slctHeaders)("Posted By"),
-              <.div(LftcontainerCSS.Style.slctleftcontentdiv, LftcontainerCSS.Style.resizable, ^.id := "resizablecontainerskills")(
-              ),
-              <.button(^.tpe := "button", ^.className := "btn btn-default", DashBoardCSS.Style.floatRightbtn, "Search")
-            ))
-      })
+              )
+            )
+          ),
+          <.div(DashBoardCSS.Style.slctHeaders)("Posted By"),
+          <.div(LftcontainerCSS.Style.slctleftcontentdiv, LftcontainerCSS.Style.resizable, ^.id := "resizablecontainerskills")(
+          ),
+          <.button(^.tpe := "button", ^.className := "btn btn-default", DashBoardCSS.Style.floatRightbtn, "Search")
+        )
+      )
+    )
     .componentDidMount(scope => Callback {
 
     })
