64e4abbfd450c0a051482ecfdb480b0c3f48f361
Ed Eykholt <ed@livelygig.com>
2016-02-02 17:28:12 -0800

    - Initial rough content for Software Credits (licenses).

--- ProductWebScalaReact/client/src/main/scala/livelygig/client/Modals/TrademarksModal.scala
+++ ProductWebScalaReact/client/src/main/scala/livelygig/client/Modals/TrademarksModal.scala
@@ -15,14 +15,17 @@ import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.prefix_<^._
 import livelygig.client.components.Bootstrap._
 import livelygig.client.components._
-import livelygig.client.css.{DashBoardCSS,MessagesCSS}
+import livelygig.client.css.{DashBoardCSS, MessagesCSS}
+
 /**
   * Created by bhagyashree.b on 1/18/2016.
   */
 object TrademarksModal {
   // shorthand for styles
   @inline private def bss = GlobalStyles.bootstrapStyles
+
   case class Props(submitHandler: () => Callback)
+
   case class State()
 
   case class Backend(t: BackendScope[Props, State]) {
@@ -30,6 +33,7 @@ object TrademarksModal {
       // instruct Bootstrap to hide the modal
       jQuery(t.getDOMNode()).modal("hide")
     }
+
     def mounted(props: Props): Callback = Callback {
     }
 
@@ -44,36 +48,78 @@ object TrademarksModal {
 
     def render(s: State, p: Props) = {
 
-      val headerText = "Trademarks"
+      val headerText = "Trademarks and Credits"
       Modal(Modal.Props(
         // header contains a cancel button (X)
         header = hide => <.span(<.button(^.tpe := "button", bss.close, ^.onClick --> hide, Icon.close), <.div(DashBoardCSS.Style.modalHeaderText)(headerText)),
         // this is called after the modal has been hidden (animation is completed)
         closed = () => formClosed(s, p)),
-        <.div(^.className:="row")(
-          //<.div(^.className:="col-md-12 col-sm-12")(<.div(DashBoardCSS.Style.modalHeaderFont,MessagesCSS.Style.paddingLeftModalHeaderbtn)("Trademarks"))
-        ),//main row
-        <.div(^.className:="row" , DashBoardCSS.Style.MarginLeftchkproduct)(
+        <.div(^.className := "row")(
+          <.div(^.className := "col-md-12 col-sm-12")(
+            <.div(DashBoardCSS.Style.modalHeaderFont, MessagesCSS.Style.paddingLeftModalHeaderbtn)("Trademarks")
+          )
+        ), //main row
+        <.div(^.className := "row", DashBoardCSS.Style.MarginLeftchkproduct)(
           <.div(DashBoardCSS.Style.marginTop10px)(
           ),
           <.div()(
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+            <.span(^.fontWeight := "bold")("Sofwtare Credits"),
+            <.br(),
+            "The development of this software was made possible using the following components:",
+            <.br(),
+            <.div(^.className := "col-sm-1")(),
+            <.div()(^.className := "col-sm-11")(
+              <.div()(
+                <.hr(),
+                <.a()(^.href := "https://github.com/synereo/special-k", ^.target := "_blank")("Special-K"),
+                " by Biosimilarity, LLC.", " TBD",
+                <.br(),
+                "Licensed Under: ",
+                <.a()(^.href := "https://www.tldrlegal.com/l/apache2", ^.target := "_blank")("Apache 2.0 TBD")
+              ),
+              <.div()(
+                <.hr(),
+                <.a()(^.href := "https://github.com/synereo/agent-service-ati-ia", ^.target := "_blank")("Agent Service ATI IA"),
+                " by Biosimilarity, LLC.", " TBD",
+                <.br(),
+                "Licensed Under: ",
+                <.a()(^.href := "https://www.tldrlegal.com/l/apache2", ^.target := "_blank")("Apache 2.0 TBD")
+              ),
+              <.div()(
+                <.hr(),
+                <.a()(^.href := "https://github.com/mongodb/mongo", ^.target := "_blank")("MongoDB"),
+                //" by ???.",
+                <.br(),
+                "Licensed Under: ",
+                <.a()(^.href := "https://www.tldrlegal.com/l/apache2", ^.target := "_blank")("Apache 2.0")
+              ),
+              <.div()(
+                <.hr(),
+                <.a()(^.href := "https://github.com/rabbitmq", ^.target := "_blank")("RabbitMQ"),
+                " (c) Pivotal Software Inc., 2007-2016",
+                <.br(),
+                "Licensed Under: ",
+                <.a()(^.href := "https://tldrlegal.com/license/mozilla-public-license-2.0-(mpl-2)", ^.target := "_blank")("MPL 2.0")
+              )
+            )
           )
         ),
         <.div()(
-          <.div(DashBoardCSS.Style.modalHeaderPadding,DashBoardCSS.Style.footTextAlign)(
+          <.div(DashBoardCSS.Style.modalHeaderPadding, DashBoardCSS.Style.footTextAlign)(
             //              <.button(^.tpe := "button",^.className:="btn btn-default", DashBoardCSS.Style.marginLeftCloseBtn, ^.onClick --> hide,"Post"),
             //              <.button(^.tpe := "button",^.className:="btn btn-default", DashBoardCSS.Style.marginLeftCloseBtn, ^.onClick --> hide,"Cancel")
           )
         ),
-        <.div(bss.modal.footer,DashBoardCSS.Style.marginTop10px,DashBoardCSS.Style.marginLeftRight)()
+        <.div(bss.modal.footer, DashBoardCSS.Style.marginTop10px, DashBoardCSS.Style.marginLeftRight)()
       )
     }
   }
+
   private val component = ReactComponentB[Props]("Trademarks")
     .initialState_P(p => State())
     .renderBackend[Backend]
     .build
+
   def apply(props: Props) = component(props)
 }
 
