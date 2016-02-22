64290236b281aadd36b2a202cdb0d5a65b1191a2
Shubham Kamthania <shubham.kamthania@afourtech.com>
2016-02-22 18:56:45 +0530

    Adding facade for prolog parsing.

--- ProductWebScalaReact/client/src/main/scala/livelygig/client/Handlers/UserHandler.scala
+++ ProductWebScalaReact/client/src/main/scala/livelygig/client/Handlers/UserHandler.scala
@@ -2,6 +2,7 @@ package livelygig.client.Handlers
 
 import diode.{ActionHandler, ModelRW}
 import livelygig.client.RootModels.UserRootModel
+import livelygig.client.components.PrologParser
 import livelygig.client.models.{UserModel}
 import org.scalajs.dom.window
 import concurrent._
@@ -16,6 +17,17 @@ case class LogoutUser()
 class UserHandler[M](modelRW: ModelRW[M, UserModel]) extends ActionHandler(modelRW) {
   override def handle = {
     case LoginUser(userModel) =>
+      /*var json = Seq[String]( "node(text(\"RocketHub\"), display(color(\"#5C9BCC\"), image(\"\")), progeny(leaf(text(\"Testimonial\"), display(color(\"#CC5C64\"), image(\"\"))), leaf(text(\"Ask\"), display(color(\"#5CCC8C\"), image(\"\"))), leaf(text(\"WhatIsSplicious\"), display(color(\"#5C64CC\"), image(\"\")))))",
+        "leaf(text(\"PopularLabels\"), display(color(\"#5C9BCC\"), image(\"\")))",
+        "leaf(text(\"Splicious\"), display(color(\"#CC5C64\"), image(\"\")))",
+        "leaf(text(\"Synereo\"), display(color(\"#C45CCC\"), image(\"\")))",
+        "leaf(text(\"VancouverDecentral\"), display(color(\"#E9CEB9\"), image(\"\")))",
+        "leaf(text(\"Crowdsale2Pt0\"), display(color(\"#CC5C64\"), image(\"\")))")
+
+
+      for(test<-json){
+        val test2 = PrologParser.StringToLabel(test)
+      }*/
       var modelFromStore = userModel
       val temp = window.sessionStorage.getItem("userEmail")
       if (temp!=null) {
---
+++ ProductWebScalaReact/client/src/main/scala/livelygig/client/components/PrologParser.scala
@@ -0,0 +1,14 @@
+package livelygig.client.components
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSName
+
+/**
+  * Created by shubham.k on 2/22/2016.
+  */
+@JSName("prologParser")
+@js.native
+object PrologParser extends js.Object {
+  def StringToLabel (string: String): js.Any = js.native
+
+}
