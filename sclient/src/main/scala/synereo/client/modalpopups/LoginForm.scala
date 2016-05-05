5e783cacf375fe511a4afd2b5064518dbeb49a37
Mandar Kawtakwar <mandar.kawtakwar@afourtech.com>
2016-05-05 15:15:34 +0530

    Added full post view modal dismiss button on scroll

--- sclient/src/main/scala/synereo/client/modalpopups/LoginForm.scala
+++ sclient/src/main/scala/synereo/client/modalpopups/LoginForm.scala
@@ -32,11 +32,11 @@ object LoginForm {
   class Backend(t: BackendScope[Props, State]) {
     def submitForm(e: ReactEventI) = {
       e.preventDefault()
-      val LoginID :js.Object ="#LoginID"
-      if($(LoginID).hasClass("disabled"))
-      t.modState(s => s.copy(login = false))
+      val LoginID: js.Object = "#LoginID"
+      if ($(LoginID).hasClass("disabled"))
+        t.modState(s => s.copy(login = false))
       else
-      t.modState(s => s.copy(login = true))
+        t.modState(s => s.copy(login = true))
     }
 
     def addNewUserForm(): Callback = {
@@ -83,8 +83,8 @@ object LoginForm {
         header = hide => <.span(<.button(^.tpe := "button", ^.className := "hide", bss.close, ^.onClick --> hide, Icon.close), <.div(/*SignupCSS.Style.signUpHeading*/)(/*headerText*/)), /*<.div()(headerText)),*/
 
         closed = () => formClosed(s, p),
-        addStyles = Seq(LoginCSS.Style.loginModalStyle)),
-        <.form(^.id:="LoginForm","data-toggle".reactAttr := "validator", ^.role:="form",^.onSubmit ==> submitForm)(
+        addStyles = Seq(LoginCSS.Style.loginModalStyle), keyboard = false),
+        <.form(^.id := "LoginForm", "data-toggle".reactAttr := "validator", ^.role := "form", ^.onSubmit ==> submitForm)(
           <.div(^.className := "row")(
             <.div(^.className := "col-md-12")(
               <.div()(
@@ -94,15 +94,15 @@ object LoginForm {
                 else
                   <.div(),
                 <.div(SignupCSS.Style.signUpHeading)(headerText),
-                  <.div(^.className:="form-group")(
-                    <.input(SignupCSS.Style.inputStyleSignUpForm, ^.tpe := "text", bss.formControl, ^.id := "Name", ^.className:="form-control","data-error".reactAttr:="Bruh, that email address is invalid",
-                    ^.placeholder := "username","data-error".reactAttr:="Username is required", ^.value := s.userModel.email, ^.onChange ==> updateEmail, ^.required := true),
-                    <.div(^.className:="help-block with-errors")
-                  ),
-                <.div(^.className:="form-group")(
-                <.input(SignupCSS.Style.inputStyleSignUpForm, ^.tpe := "password", bss.formControl, ^.placeholder := "password",^.className:="form-control",^.id:="inputPassword","data-error".reactAttr:="Password is required",
-                   ^.value := s.userModel.password, ^.onChange ==> updatePassword,  ^.required := true),
-                  <.div(^.className:="help-block with-errors")
+                <.div(^.className := "form-group")(
+                  <.input(SignupCSS.Style.inputStyleSignUpForm, ^.tpe := "text", bss.formControl, ^.id := "Name", ^.className := "form-control", "data-error".reactAttr := "Bruh, that email address is invalid",
+                    ^.placeholder := "username", "data-error".reactAttr := "Username is required", ^.value := s.userModel.email, ^.onChange ==> updateEmail, ^.required := true),
+                  <.div(^.className := "help-block with-errors")
+                ),
+                <.div(^.className := "form-group")(
+                  <.input(SignupCSS.Style.inputStyleSignUpForm, ^.tpe := "password", bss.formControl, ^.placeholder := "password", ^.className := "form-control", ^.id := "inputPassword", "data-error".reactAttr := "Password is required",
+                    ^.value := s.userModel.password, ^.onChange ==> updatePassword, ^.required := true),
+                  <.div(^.className := "help-block with-errors")
                 ),
                 <.div(^.className := "row")(
                   <.div(^.className := "col-md-6 text-left", LoginCSS.Style.loginModalTextActionContainer)(
@@ -113,8 +113,8 @@ object LoginForm {
                     <.a(^.href := "", LoginCSS.Style.loginModalTextStyle)("Forgot Password?")
                   )
                 ),
-                <.div(^.className := "text-center",^.className:="form-group")(
-                  <.button(^.tpe := "submit",^.id:="LoginID", LoginCSS.Style.modalLoginBtn, ^.className := "btn", "Login")
+                <.div(^.className := "text-center", ^.className := "form-group")(
+                  <.button(^.tpe := "submit", ^.id := "LoginID", LoginCSS.Style.modalLoginBtn, ^.className := "btn", "Login")
                 )
               )
             )
