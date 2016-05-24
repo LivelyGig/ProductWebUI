227724693477df1f4eb5c19964b3dfdab5ddaa69
Shubham Kamthania <shubham.kamthania@afourtech.com>
2016-05-24 20:03:00 +0530

    Removing proxy setup :)

--- client/src/main/scala/client/services/CoreApi.scala
+++ client/src/main/scala/client/services/CoreApi.scala
@@ -11,44 +11,45 @@ import scala.concurrent.Future
 import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
 import autowire._
 import boopickle.Default._
+import org.scalajs.dom.ext.Ajax
 import shared.sessionitems.SessionItems
 
 object CoreApi {
-  //  var BASE_URL = "http://52.35.10.219:9876/api"
-  //  var CREATE_USER_REQUEST = "createUserRequest"
-  //  private def ajaxPost(msgType: String, data: RequestContent): Future[String] = {
-  //    Ajax.post(
-  //      url = BASE_URL,
-  //      data = write(ApiRequest(msgType, data)),
-  //      headers = Map("Content-Type" -> "application/json;charset=UTF-8")
-  //    ).map(_.responseText)
-  //  }
+    var BASE_URL = "http://192.168.99.100:8888/api"
+    var CREATE_USER_REQUEST = "createUserRequest"
+    private def ajaxPost(requestContent: String): Future[String] = {
+      Ajax.post(
+        url = BASE_URL,
+        data = requestContent,
+        headers = Map("Content-Type" -> "application/json;charset=UTF-8")
+      ).map(_.responseText)
+    }
 
   def getConnections(): Future[String] = {
     val requestContent = upickle.default.write(ApiRequest(ApiTypes.SESSION_PING, SessionPing(window.sessionStorage.getItem(SessionItems.ConnectionViewItems.CONNECTIONS_SESSION_URI))))
-    AjaxClient[Api].queryApiBackend(requestContent).call()
+    ajaxPost(requestContent)
   }
 
   def createUser(signUpModel: SignUpModel): Future[String] = {
     val requestContent = upickle.default.write(ApiRequest(ApiTypes.CREATE_USER_REQUEST, CreateUser(signUpModel.email, signUpModel.password,
       Map("name" -> signUpModel.name), true)))
-    AjaxClient[Api].queryApiBackend(requestContent).call()
+    ajaxPost(requestContent)
   }
 
   def emailValidation(emailValidationModel: EmailValidationModel): Future[String] = {
     val requestContent = upickle.default.write(ApiRequest(ApiTypes.CONFIRM_EMAIL, ConfirmEmail(emailValidationModel.token)))
-    AjaxClient[Api].queryApiBackend(requestContent).call()
+    ajaxPost(requestContent)
   }
 
   def agentLogin(userModel: UserModel): Future[String] = {
     val requestContent = upickle.default.write(ApiRequest(ApiTypes.INITIALIZE_SESSION_REQUEST, InitializeSession(s"agent://email/${userModel.email}" +
       s"?password=${userModel.password}")))
-    AjaxClient[Api].queryApiBackend(requestContent).call()
+    ajaxPost(requestContent)
   }
 
   def sessionPing(uri: String): Future[String] = {
     val requestContent = upickle.default.write(ApiRequest(ApiTypes.SESSION_PING, SessionPing(uri)))
-    AjaxClient[Api].queryApiBackend(requestContent).call()
+    ajaxPost(requestContent)
   }
 
   def getContent(sessionUriName: String): Future[String] = {
@@ -78,7 +79,7 @@ object CoreApi {
 
   def evalSubscribeRequest(subscribeRequest: SubscribeRequest): Future[String] = {
     val requestContent = upickle.default.write(ApiRequest(ApiTypes.EVAL_SUBS_REQUEST, subscribeRequest))
-    AjaxClient[Api].queryApiBackend(requestContent).call()
+    ajaxPost(requestContent)
   }
 
   def evalSubscribeRequestAndSessionPing(subscribeRequest: SubscribeRequest): Future[String] = {
@@ -97,7 +98,7 @@ object CoreApi {
 
   def cancelSubscriptionRequest(cancelSubscribeRequest: CancelSubscribeRequest): Future[String] = {
     val requestContent = upickle.default.write(ApiRequest(ApiTypes.EVAL_SUBS_CANCEL_REQUEST, cancelSubscribeRequest))
-    AjaxClient[Api].queryApiBackend(requestContent).call()
+    ajaxPost(requestContent)
   }
 
 }
