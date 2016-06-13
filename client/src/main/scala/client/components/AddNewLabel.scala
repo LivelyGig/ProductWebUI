efc9165d0551072935fa0d4159180125f5c6bf69
Bhagyashree Birajdar <bhagyashree.birajdar@afourtech.com>
2016-06-13 14:55:37 +0530

    Adding inline form for label post.

--- client/src/main/scala/client/components/AddNewLabel.scala
+++ client/src/main/scala/client/components/AddNewLabel.scala
@@ -46,16 +46,15 @@ import org.querki.jquery._
 //}
 
 object LabelsList {
+  val addBtn : js.Object = "#addBtn"
 
-  def addCollapsibleBtn : Callback = Callback{
-//    val addLabel : js.Object = "#addLabel"
-//    val addBtn : js.Object = "#addBtn"
-//    if($(addLabel).is(":visible"))
-//      println("true")
-//    else
-//      println("false")
+  def addCollapsibleHideBtn : Callback = Callback{
+    $(addBtn).hide()
   }
 
+  def addCollapsibleShowBtn : Callback = Callback{
+    $(addBtn).show()
+  }
 
   case class Props(proxy: ModelProxy[SearchesRootModel])
 
@@ -64,26 +63,26 @@ object LabelsList {
   case class Backend(t: BackendScope[Props, State]) {
 
     def mounted(props: Props) = {
-      addCollapsibleBtn
+
     }
 
     def updateLabel(e: ReactEventI): react.Callback = {
       val value = e.currentTarget.value
       println("Updated Label " + value)
       t.modState(s => s.copy(labelModel = s.labelModel.copy(text = value)))
-
     }
 
     def postLabel(e: ReactEventI) = {
       e.preventDefault()
-      val label = t.state.runNow().labelModel
+      var label = t.state.runNow().labelModel
       println(s"On submit label text: ${label.text}")
+      $(addBtn).show()
       t.modState(s => s.copy(postLabel = !s.postLabel))
     }
 
     def render(props: Props, state: State) = {
       <.div(
-        <.button(^.id:="addBtn",^.`type` := "button", ^.className := "btn btn-default", "data-toggle".reactAttr := "collapse", "data-target".reactAttr := "#addLabel")("Add Label"),
+        <.button(^.id:="addBtn",^.`type` := "button", ^.className := "btn btn-default", "data-toggle".reactAttr := "collapse", "data-target".reactAttr := "#addLabel",^.onClick --> addCollapsibleHideBtn)("Add Label"),
         <.div(^.id := "addLabel", ^.className := "collapse")(
           //          <.div(^.className := "form-group")(
           //            <.label(^.`for` := "parent")("Parent"),
@@ -96,7 +95,7 @@ object LabelsList {
           <.br,
           <.div(^.className:="input-group")(
           <.input(^.`type` := "text", ^.className := "form-control", ^.value := state.labelModel.text, ^.onChange ==> updateLabel),
-          <.span(^.className:="input-group-addon",^.`type` := "button", "data-toggle".reactAttr := "collapse", "data-target".reactAttr := "#addLabel",^.className := "btn")(Icon.times),
+          <.span(^.className:="input-group-addon",^.`type` := "button", "data-toggle".reactAttr := "collapse", "data-target".reactAttr := "#addLabel",^.className := "btn",^.onClick-->addCollapsibleShowBtn)(Icon.times),
           <.span(^.className:="input-group-addon",^.`type` := "button", "data-toggle".reactAttr := "collapse", "data-target".reactAttr := "#addLabel",^.className := "btn", ^.onClick ==> postLabel)(Icon.check)
           )
         )
@@ -107,10 +106,9 @@ object LabelsList {
   val component = ReactComponentB[Props]("getAllSearchLabels")
     .initialState(State(LabelModel()))
     .renderBackend[Backend]
-    .componentDidMount(scope => scope.backend.mounted(scope.props))
+   //.componentDidMount(scope => scope.backend.mounted(scope.props))
     .build
 
   def apply(props: Props) = component(props)
 
-  //  def apply: getAllSearchLabels = new getAllSearchLabels()
 }
