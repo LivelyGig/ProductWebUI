8d994fa056f97a1db49d1d309aa69c9b4e4c21cc
Bhagyashree Birajdar <bhagyashree.birajdar@afourtech.com>
2016-08-22 10:56:21 +0530

    Removed commented code from Coreapi.scala

--- client/src/main/scala/client/services/CoreApi.scala
+++ client/src/main/scala/client/services/CoreApi.scala
@@ -4,15 +4,8 @@ import shared.dtos._
 import org.scalajs.dom._
 import upickle.default._
 
-import scala.concurrent.Future
-import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
-import org.scalajs.dom.ext.Ajax
 import shared.models.{EmailValidationModel, SignUpModel, UserModel}
-import client.sessionitems.SessionItems
 import client.srp.SRPClient
-
-
-
 import scala.concurrent.Future
 import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
 import scala.scalajs.js.Date
@@ -21,8 +14,6 @@ import scala.language.postfixOps
 import org.scalajs.dom.ext.Ajax
 import shared.models.{EmailValidationModel, SignUpModel, UserModel}
 import client.sessionitems.SessionItems
-//import shared.sessionitems.SessionItems
-//import shared.sessionitems.SessionItems.ProfilesViewItems
 import client.modules.{ConnectionList}
 import client.utils.{ConnectionsUtils, LabelsUtils}
 
@@ -35,15 +26,11 @@ object CoreApi {
     s"https://${window.sessionStorage.getItem(SessionItems.ApiDetails.API_HOST)}:${window.sessionStorage.getItem(SessionItems.ApiDetails.API_PORT)}/api"
   }
 
-//  def getBaseUrl() = {
-//    s"${window.sessionStorage.getItem(SessionItems.ApiDetails.API_URL)}/api"
-//  }
+
 
   // scalastyle:ignore
   var CREATE_USER_REQUEST = "createUserRequest"
 
-//  // scalastyle:ignore
-//  var CREATE_USER_REQUEST = "createUserRequest"
 
   private def ajaxPost(requestContent: String): Future[String] = {
     Ajax.post(
@@ -53,23 +40,6 @@ object CoreApi {
     ).map(_.responseText)
 
   }
-
-//  private def ajaxPost(requestContent: String): Future[String] = {
-//    Ajax.post(
-//      url = getBaseUrl,
-//      data = requestContent,
-//      headers = Map("Content-Type" -> "application/json;charset=UTF-8")
-//    ).map(_.responseText)
-//
-//  }
-
-
-  /* LivelyGig
-  def createUser(signUpModel: SignUpModel): Future[String] = {
-    val requestContent = upickle.default.write(ApiRequest(ApiTypes.CREATE_USER_REQUEST, CreateUser(signUpModel.email, signUpModel.password,
-      Map("name" -> signUpModel.name), true)))
-    ajaxPost(requestContent)
-  }*/
 
   def createUser(signUpModel: SignUpModel): Future[String] = {
     val srpc = new SRPClient(signUpModel.email, signUpModel.password)
@@ -99,17 +69,6 @@ object CoreApi {
     ajaxPost(requestContent)
   }
 
-//  def emailValidation(emailValidationModel: EmailValidationModel): Future[String] = {
-//    val requestContent = upickle.default.write(ApiRequest(ApiTypes.CONFIRM_EMAIL, ConfirmEmail(emailValidationModel.token)))
-//    ajaxPost(requestContent)
-//  }
-
-/* LivelyGIg
-  def agentLogin(userModel: UserModel): Future[String] = {
-    val requestContent = upickle.default.write(ApiRequest(ApiTypes.INITIALIZE_SESSION_REQUEST, InitializeSession(s"agent://email/${userModel.email}" +
-      s"?password=${userModel.password}")))
-    ajaxPost(requestContent)
-  }*/
 
   def agentLogin(userModel: UserModel): Future[String] = {
     val srpc = new SRPClient(userModel.email, userModel.password)
@@ -148,10 +107,7 @@ object CoreApi {
     ajaxPost(requestContent)
   }
 
-//  def sessionPing(uri: String): Future[String] = {
-//    val requestContent = upickle.default.write(ApiRequest(ApiTypes.SESSION_PING, SessionPing(uri)))
-//    ajaxPost(requestContent)
-//  }
+
 
   def evalSubscribeRequest(subscribeRequest: SubscribeRequest): Future[String] = {
     val requestContent = upickle.default.write(ApiRequest(ApiTypes.EVAL_SUBS_REQUEST, subscribeRequest))
@@ -159,22 +115,11 @@ object CoreApi {
   }
 
 
-
-//  def evalSubscribeRequest(subscribeRequest: SubscribeRequest): Future[String] = {
-//    val requestContent = upickle.default.write(ApiRequest(ApiTypes.EVAL_SUBS_REQUEST, subscribeRequest))
-//    ajaxPost(requestContent)
-//  }
-
   def cancelSubscriptionRequest(cancelSubscribeRequest: CancelSubscribeRequest): Future[String] = {
     val requestContent = upickle.default.write(ApiRequest(ApiTypes.EVAL_SUBS_CANCEL_REQUEST, cancelSubscribeRequest))
     ajaxPost(requestContent)
   }
 
-
-//  def cancelSubscriptionRequest(cancelSubscribeRequest: CancelSubscribeRequest): Future[String] = {
-//    val requestContent = upickle.default.write(ApiRequest(ApiTypes.EVAL_SUBS_CANCEL_REQUEST, cancelSubscribeRequest))
-//    ajaxPost(requestContent)
-//  }
 
   def postIntroduction(introductionModel: Content): Future[String] = {
     val msg = introductionModel match {
@@ -187,34 +132,15 @@ object CoreApi {
 
 
 
-//  def postIntroduction(introductionModel: Content): Future[String] = {
-//    val msg = introductionModel match {
-//      case _: IntroConnections => ApiTypes.BEGIN_INTRODUCTION_REQUEST
-//      case _: EstablishConnection => ApiTypes.ESTABLISH_CONNECTION_REQ
-//      case _: IntroConfirmReq => ApiTypes.INTRODUCTION_CONFIRMATION_REQUEST
-//    }
-//    ajaxPost(upickle.default.write(ApiRequest(msg, introductionModel)))
-//  }
-
   def postLabel(labelPost: LabelPost): Future[String] = {
     val requestContent = upickle.default.write(ApiRequest(ApiTypes.UPDATE_ALIAS_LABEL_REQ, labelPost))
     ajaxPost(requestContent)
   }
-
-
-//  def postLabel(labelPost: LabelPost): Future[String] = {
-//    val requestContent = upickle.default.write(ApiRequest(ApiTypes.UPDATE_ALIAS_LABEL_REQ, labelPost))
-//    ajaxPost(requestContent)
-//  }
 
   def updateUserRequest(updateUserRequest: UpdateUserRequest): Future[String] = {
     val requestContent = upickle.default.write(ApiRequest(ApiTypes.UPDATE_USER_REQUEST, updateUserRequest))
     ajaxPost(requestContent)
   }
 
-//  def updateUserRequest(updateUserRequest: UpdateUserRequest): Future[String] = {
-//    val requestContent = upickle.default.write(ApiRequest(ApiTypes.UPDATE_USER_REQUEST, updateUserRequest))
-//    ajaxPost(requestContent)
-//  }
 
 }
