3b4a7dd3add905f325921dbdb8d04c9c7acb53fd
Shubham Kamthania <shubham.kamthania@afourtech.com>
2016-11-14 16:53:37 +0530

    Adding appveyour ci

---
+++ scripts/appveyour.ps1
@@ -0,0 +1,21 @@
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+# SBT
+
+$sbtZipPath = "$($env:USERPROFILE)\sbt-0.13.12.zip"
+
+if (!(Test-Path -Path $sbtZipPath)) {
+    Write-Host "Downloading sbt.." -ForegroundColor Cyan
+    (new-object System.Net.WebClient).DownloadFile('https://dl.bintray.com/sbt/native-packages/sbt/0.13.12/sbt-0.13.12.zip', $sbtZipPath)
+}
+
+Write-Host "Installing sbt..."
+[System.IO.Compression.ZipFile]::ExtractToDirectory($sbtZipPath, "C:\")
+
+Write-Host "sbt installed" -ForegroundColor Green
+
+# Node
+
+Install-Product node $env:nodejs_version
+
+npm install
